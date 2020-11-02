6610c4220ec2672e466faef107d8f7fad0c85324
Eason-Life <240249923@qq.com>
2020-11-02 21:34:16 +0800

    new file Hello3

---
+++ src/main/java/com/yufan/gitxuexi/Hello3.java
@@ -0,0 +1,13 @@
+package com.yufan.gitxuexi;
+
+/**
+ * @author YuFan
+ * @date 2020/11/1 2:18
+ * @jdk version 1.8.0_251
+ */
+public class Hello3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello 3");
+    }
+}
